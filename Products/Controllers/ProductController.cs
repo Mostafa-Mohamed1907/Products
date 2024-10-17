23a7b298d1982257fcb4289bd9ef41e1a7e4ebca
Mostafa Mohamed <mostafa.mohamed851@yahoo.com>
2024-10-17 15:09:50 +0200

    Add project files.

---
+++ Products/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Products.Models;
+using System.Net;
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        //[HttpGet]
+        //public List<Product> GetAll() 
+        //{
+        //    var products = Context.Products.ToList();
+        //    return products;
+        //}
+        //[HttpGet ("{id}")]
+        //public Product GetById(int id)
+        //{
+        //    var product = Context.Products.SingleOrDefault(p => p.Id == id);
+        //    return product;
+        //}
+        //[HttpGet("name/{name}")]
+        //public Product GetByName(string name)
+        //{
+        //    var product = Context.Products.SingleOrDefault(p => p.Name.Contains(name));
+        //    return product;
+        //}
+        //[HttpPost]
+        //public HttpResponseMessage CreateProduct([FromBody] Product product)
+        //{
+        //    Context.Products.Add(product);
+        //    var response = new HttpResponseMessage(HttpStatusCode.Created);
+        //    return response;
+        //}
+        //[HttpPut("id/{id}")]
+        //public HttpResponseMessage UpdateProduct(int id, [FromBody] Product product)
+        //{
+        //    var ExistingProduct = Context.Products.FirstOrDefault(p => p.Id == id);
+        //    ExistingProduct.Name= product.Name;
+        //    ExistingProduct.Description = product.Description;
+        //    ExistingProduct.Price=product.Price;
+        //    return new HttpResponseMessage(HttpStatusCode.NoContent);
+        //}
+        //[HttpDelete]
+        //public HttpResponseMessage DeleteProduct(int id)
+        //{
+        //    var ExistingProduct = Context.Products.FirstOrDefault(p => p.Id == id);
+        //    Context.Products.Remove(ExistingProduct);
+        //    return new HttpResponseMessage(HttpStatusCode.NoContent);
+        //}
+
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetAll()
+        {
+            var products = Context.Products.ToList();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetById(int id)
+        {
+            var product = Context.Products.SingleOrDefault(p => p.Id == id);
+            if (product == null) { return NotFound(); }
+            return Ok(product);
+        }
+        [HttpGet("name/{name}")]
+        public ActionResult<Product> GetByName(string name)
+        {
+            var product = Context.Products.SingleOrDefault(p => p.Name.Contains(name));
+            if (product == null) { return NotFound(); }
+            return Ok(product);
+        }
+        [HttpPost]
+        public ActionResult<Product> CreateProduct([FromBody] Product product)
+        {
+            Context.Products.Add(product);
+            return CreatedAtAction(nameof(GetById), new { id = product.Id, product });
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, [FromBody] Product product)
+        {
+            var ExistingProduct = Context.Products.FirstOrDefault(p => p.Id == id);
+            if (ExistingProduct == null) { return NotFound(); }
+            ExistingProduct.Name = product.Name;
+            ExistingProduct.Description = product.Description;
+            ExistingProduct.Price = product.Price;
+            return NoContent();
+        }
+        [HttpDelete]
+        public IActionResult DeleteProduct(int id)
+        {
+            var ExistingProduct = Context.Products.FirstOrDefault(p => p.Id == id);
+            if (ExistingProduct == null) { return NotFound(); }
+            Context.Products.Remove(ExistingProduct);
+            return NoContent();
+        }
+    }
+}
---
+++ Products/Models/Context.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Models
+{
+    public class Context
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product {Id=1,Name="Laptop",Price=2000,Description="New Laptop"},
+            new Product {Id=2,Name="SmartPhone",Price=1000,Description="Used"},
+            new Product {Id=3,Name="Tablet",Price=2500,Description="New Tablet"},
+        };
+    }
+}
